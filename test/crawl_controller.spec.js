81b99955fa49a9c052c2cf544136e22262ad9a9c
Paul Cleary <pauljohncleary@gmail.com>
2015-03-22 21:00:08 +0000

    added test for crawl_controller

---
+++ test/crawl_controller.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expectIt = require('chai').expect,
+  crawl_controller = require('../lib/crawl_controller.js');
+
+describe('Controller', function(){
+
+  it('should start a crawl when supplied with a url root', function(){
+    return crawl_controller.startCrawl('http://tab.bz')
+      .then(function(data) {
+        expectIt(data).to.exist();
+      });
+  });
+});
