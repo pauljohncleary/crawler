14805d2a384eedc7ff6b4f7a5d2e306ef07f752b
Paul Cleary <pauljohncleary@gmail.com>
2015-04-02 23:36:38 +0100

    added error handling to pinger

--- test/crawl_controller.spec.js
+++ test/crawl_controller.spec.js
@@ -6,7 +6,7 @@ var expectIt = require('chai').expect,
 describe('Controller', function(){
 
   it('should start a crawl when supplied with a url root', function(){
-    return crawl_controller.startCrawl('http://tab.bz')
+    return crawl_controller.startCrawl('http://httpstat.us/404')
       .then(function(data) {
         expectIt(data).to.exist();
       });
